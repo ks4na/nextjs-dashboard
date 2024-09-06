5de6ad676ec512b4479cc41f97207b5c4207fc61
ks4na <1017086648@qq.com>
2024-09-06 10:39:56 +0800

    fix: revert to sync with tutorial as inexist route will not redirect to login page

--- auth.config.ts
+++ auth.config.ts
@@ -7,27 +7,27 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) {
-      //     return true;
-      //   }
-      //   return false;
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
-      // return true;
-
-      const isIndexPage = nextUrl.pathname === '/';
-      const isLoginPage = nextUrl.pathname === '/login';
-      if (isIndexPage || isLoginPage) {
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
         if (isLoggedIn) {
-          return Response.redirect(new URL('/dashboard', nextUrl));
+          return true;
         }
-        return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
+      return true;
 
-      return isLoggedIn;
+      // const isIndexPage = nextUrl.pathname === '/';
+      // const isLoginPage = nextUrl.pathname === '/login';
+      // if (isIndexPage || isLoginPage) {
+      //   if (isLoggedIn) {
+      //     return Response.redirect(new URL('/dashboard', nextUrl));
+      //   }
+      //   return true;
+      // }
+
+      // return isLoggedIn;
     },
   },
   providers: [],
