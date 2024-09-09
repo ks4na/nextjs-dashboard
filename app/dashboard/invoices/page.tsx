9047b0f009443a9fe8fc0dbf2ad86a379db489fe
ks4na <1017086648@qq.com>
2024-09-09 11:34:48 +0800

    to avoid querying database

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -15,17 +15,17 @@ export default async function InvoicesPage({
     page?: string;
   };
 }) {
-  const query = searchParams?.query || '';
-  const currentPage = Number.parseInt(searchParams?.page || '') || 1;
+  // const query = searchParams?.query || '';
+  // const currentPage = Number.parseInt(searchParams?.page || '') || 1;
 
-  const totalPages = await fetchInvoicesPages(query);
+  // const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
@@ -34,7 +34,7 @@ export default async function InvoicesPage({
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
-      </div>
+      </div> */}
     </div>
   );
 }
