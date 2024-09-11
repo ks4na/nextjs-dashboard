b0ea0ab558ca0358e6fce162d78855b7aae981dd
ks4na <1017086648@qq.com>
2024-09-11 17:26:44 +0800

    use auth wrapper

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,12 @@ import { authConfig } from './auth.config';
 import { NextRequest, NextResponse } from 'next/server';
 
 const { auth } = NextAuth(authConfig);
-// export default auth(async function middleware(req: NextRequest) {
-//   // Your custom middleware logic goes here
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
 
-//   return NextResponse.next();
-// });
-export default auth;
+  return NextResponse.next();
+});
+// export default auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
